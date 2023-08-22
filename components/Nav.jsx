008b7894da35b020faa556b5af7db7513fadce4c
Calvin Chiramal <calvin.chiramal@bigbinary.com>
2023-08-22 10:13:52 +0530

    Added mobile navigation bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,6 +10,7 @@ const Nav = () => {
   const isUserLoggedIn = true;
 
   const [providers, setProviders] = useState(null);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
   useEffect(() => {
     const response = async () => await getProviders();
@@ -29,35 +30,91 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
+      {/* Desktop Navbar */}
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (
-        <div className="flex gap-3 md:gap-5">
-          <Link href="create-prompt" className="black_btn">
-            Create post
-          </Link>
-          <button className="outline_btn" onClick={signOut}>Sign Out</button>
-          <Link href="/profile">
+          <div className="flex gap-3 md:gap-5">
+            <Link href="create-prompt" className="black_btn">
+              Create post
+            </Link>
+            <button className="outline_btn" onClick={signOut}>Sign Out</button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="User profile"
+                width={37}
+                height={37}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers && providers.map(provider => (
+            <button
+              key={provider.name}
+              onClick={() => signIn(provider.id)}
+              className="black_btn"
+            >
+              Sign In
+            </button>
+            ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navbar */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
             <Image
               src="/assets/images/logo.svg"
               alt="User profile"
               width={37}
               height={37}
               className="rounded-full"
+              onClick={() => setIsDropdownOpen(prev => !prev)}
             />
-          </Link>
-        </div>
+              {isDropdownOpen && (
+                <div className="dropdown">
+                  <Link
+                    className="dropdown_link"
+                    href="/profile"
+                    onClick={() => setIsDropdownOpen(false)}
+                  >
+                    My Profile
+                  </Link>
+                  <Link
+                    className="dropdown_link"
+                    href="/create-prompt"
+                    onClick={() => setIsDropdownOpen(false)}
+                  >
+                    Create Prompt
+                  </Link>
+                  <button
+                    className="mt-5 w-full black_btn"
+                    onClick={() => {
+                      setIsDropdownOpen(false);
+                      signOut();
+                    }}
+                  >
+                    Sign out
+                  </button>
+                </div>
+              )}
+          </div>
         ) : (
-        <>
-          {providers && providers.map(provider => (
-          <button
-            key={provider.name}
-            onClick={() => signIn(provider.id)}
-            className="black_btn"
-          >
-            Sign In
-          </button>
-          ))}
-        </>
+          <>
+            {providers && providers.map(provider => (
+            <button
+              key={provider.name}
+              onClick={() => signIn(provider.id)}
+              className="black_btn"
+            >
+              Sign In
+            </button>
+            ))}
+          </>
         )}
       </div>
     </nav>
